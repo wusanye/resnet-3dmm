0a3d7d42bc2c0781cb32199a8310f052cdd90a61
wusanye <1419816717@qq.com>
2018-08-20 14:31:39 +0800

    warp some function

---
+++ train_new.py
@@ -0,0 +1,102 @@
+
+#############################################
+# Copyright (c) 2018-present
+# written by Kai Wu on 2018-07-31
+#############################################
+
+"""THIS IS THE IMPLEMENTATION OF MAIN TRAINING PROCESS"""
+
+import os
+import shutil
+import tensorflow as tf
+from collections import OrderedDict
+from resnet_new import ResNet
+from nn_layers import basic_block
+from utils import asym_l2_loss, optimize_loss, l2_loss, load_data_sets, train
+# from tensorflow.python import debug as tf_debug
+
+'''configuration part'''
+# Path to the text files for the training/val/test set
+image_size = [450, 450, 3]
+train_list = "./list/train.list"
+val_list = "./list/dev.list"
+test_list = "./test.list"
+
+# 10 epochs for seeing the effect on the whole
+epochs = 120
+# 256 for first try, try x2 or x0.5
+batch_size = 32  # 128
+# Network parameters
+output_dim = 185
+# Display frequency (print/#batch)
+display_step = 400
+
+logdir = "train_logs"
+
+if os.path.exists(logdir):
+    shutil.rmtree(logdir)
+os.makedirs(logdir)
+
+'''main part of training'''
+# # load train & validation data
+data_family = load_data_sets(image_size[0], output_dim, batch_size, train_list, val_list)
+
+# TF placeholder for graph input and output
+y = tf.placeholder(tf.float32, [None, output_dim], name='data_label')
+
+# Initialize model
+model = ResNet([224, 224, 3], output_dim, basic_block, [3, 4, 6, 3])
+
+predicts = model.predicts
+x, training = model.images, model.training
+
+with tf.name_scope("Asymmetric_L2_Loss"):
+    l2_loss = asym_l2_loss(predicts, y)
+    reg_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    loss = l2_loss + tf.add_n(reg_loss)
+
+var_list = [v for v in tf.trainable_variables()]
+
+# # annealing the learning rate with different decay curve
+global_step = tf.Variable(0, name='global_step', trainable=False)
+boundaries = [10*2000, 20*2000, 40*2000, 60*2000]
+lr_value = [0.001, 0.005, 0.001, 0.0005, 0.0001]
+lr_rate = tf.train.piecewise_constant(global_step, boundaries, lr_value)
+
+# # optimizer.minimize
+with tf.name_scope("train"):
+    optimizer = tf.train.MomentumOptimizer(learning_rate=lr_rate, momentum=0.9, use_nesterov=True)
+    grads_and_vars = optimizer.compute_gradients(loss, var_list)
+    train_op = optimizer.apply_gradients(grads_and_vars=grads_and_vars)
+    update_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS)  # batch norm update
+
+# # statistic summary
+for var in var_list:
+    tf.summary.histogram(var.name, var)
+
+for gradient, var in grads_and_vars:
+    tf.summary.histogram(var.name + '/gradient', gradient)
+
+tf.summary.scalar('lr', lr_rate)
+tf.summary.scalar('l2 loss', l2_loss)
+tf.summary.scalar('total loss', loss)
+
+saver = tf.train.Saver(max_to_keep=epochs)
+
+feed_dict = OrderedDict.fromkeys([x, y, training])
+
+train(train_op, update_op, loss, feed_dict, data_family, epochs, saver, logdir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -5,40 +5,33 @@
 
 """This is implementation of batch preprocess."""
 
+import os
+import glob
 import numpy as np
+import collections
 import tensorflow as tf
+# from os import listdir
+# from os.path import isfile, join
+from datetime import datetime
 from tensorflow.data import Dataset
+from tensorflow.data import Iterator
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework.ops import convert_to_tensor
 
 TRAIN_SET_MEAN = tf.constant([123.68, 116.779, 103.939], dtype=tf.float32)
 
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+DataMember = collections.namedtuple("DataMember", ['init_op', 'num_examples', 'batch_size'])
+
+DataFamily = collections.namedtuple("DataFamily", ['train', 'val', 'next_batch'])
+
 
 class DataGenerator(object):
-    """
-    Data set generator, with tensorflow Dataset and iterator
-    """
+
     def __init__(self, txt_file, image_size, output_dim, mode, batch_size, shuffle=True,
                  buffer_size=10000):
-        """Create a new ImageDataGenerator.
-        Receives a path string to a text file, which consists of many lines,
-        where each line has first a path string to an image and seperated by
-        a space an integer, referring to the class number. Using this data,
-        this class will create TensorFlow datasets, that can be used to train
-        e.g. a convolution neural network.
-        Args:
-            txt_file: Path to the text file.
-            mode: Either 'training' or 'validation'. Depending on this value,
-                different parsing functions will be used.
-            batch_size: Number of images per batch.
-            num_classes: Number of classes in the dataset.
-            shuffle: Whether or not to shuffle the data in the dataset and the
-                initial file list.
-            buffer_size: Number of images used as buffer for TensorFlows
-                shuffling of the dataset.
-        Raises:
-            ValueError: If an invalid mode is passed.
-        """
+
         self.txt_file = txt_file
         self.output_dim = output_dim
         self.image_size = image_size
@@ -78,7 +71,6 @@ class DataGenerator(object):
         self.data = data
 
     def _read_txt_file(self):
-        """Read the content of the text file and store it into lists."""
         self.img_paths = []
         self.labels = []
         with open(self.txt_file, 'r') as f:
@@ -91,7 +83,6 @@ class DataGenerator(object):
                 self.labels.append(num_list)
 
     def _shuffle_lists(self):
-        """Conjoined shuffling of the list of paths and labels."""
         img_paths = self.img_paths
         labels = self.labels
         permutation = np.random.permutation(self.data_size)
@@ -102,7 +93,6 @@ class DataGenerator(object):
             self.labels.append(labels[i])
 
     def _parse_function_train(self, img_file, label):
-        """input parser for samples of the training set."""
 
         img_normed = self.parse_data(img_file)
 
@@ -111,7 +101,6 @@ class DataGenerator(object):
         return img_normed, label_normed
 
     def _parse_function_inference(self, img_file, label):
-        """input parser for samples of the validation/test set."""
 
         img_normed = self.parse_data(img_file)
 
@@ -121,7 +110,6 @@ class DataGenerator(object):
 
     @staticmethod
     def parse_data(img_file):
-
         # load and pre-process the image
         img_string = tf.read_file(img_file)
         img_decoded = tf.image.decode_jpeg(img_string, channels=3)
@@ -141,7 +129,6 @@ class DataGenerator(object):
 
 
 def label_norm(image_size):
-
     f = np.load('bfm09/std_shape_exp.npz')
     shape_std, exp_std = f['shape_ev'], f['exp_ev']
     rest_std = np.array([1, 1, 1, image_size, image_size, image_size/224.], dtype=np.float32)
@@ -220,7 +207,119 @@ def optimize_loss(train_optimizer, lr_rate, predicts, truths, summary=True):
     return train_op, update_op, loss
 
 
+def load_data_sets(image_size, output_dim, batch_size, train_list, val_list):
 
+    # Place data loading and pre-processing on cpu
+    with tf.device('/cpu:0'):
+        train_data = DataGenerator(train_list, image_size[0], output_dim, 'training', batch_size, shuffle=True)
+        val_data = DataGenerator(val_list, image_size[0], output_dim, 'inference', batch_size, shuffle=False)
+
+    # Create an reinitializable iterator given the data structure
+    iterator = Iterator.from_structure(train_data.data.output_types, train_data.data.output_shapes)
+    next_batch = iterator.get_next()
+
+    # Ops for initializing the two different iterators
+    train_init_op = iterator.make_initializer(train_data.data)
+    val_init_op = iterator.make_initializer(val_data.data)
+
+    train_member = DataMember(init_op=train_init_op, num_examples=train_data.data_size, batch_size=batch_size)
+    val_member = DataMember(init_op=val_init_op, num_examples=val_data.data_size, batch_size=batch_size)
+
+    return DataFamily(train=train_member, val=val_member, next_batch=next_batch)
+
+
+def load_data_set():
+    pass
+
+
+def train(train_op, update_op, loss, feeds, dataset, epochs, saver, logdir, display_step=100):
+
+    merged_summary = tf.summary.merge_all()
+
+    train_writer = tf.summary.FileWriter(logdir + '/train')
+    dev_writer = tf.summary.FileWriter(logdir + '/dev')
+
+    train_batches = np.ceil(dataset.train.num_examples / dataset.train.batch_size)
+    val_batches = np.ceil(dataset.val.num_examples / dataset.val.bnatch_size)
+
+    train_init_op = dataset.train.init_op
+    val_init_op = dataset.val.init_op
+
+    next_batch = dataset.next_batch
+
+    # # begin training
+    with tf.Session() as sess:
+        # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+        # sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
+
+        sess.run(tf.global_variables_initializer())
+
+        train_writer.add_graph(sess.graph)
+
+        print("{} begin training...".format(datetime.now().strftime(TIME_FORMAT)))
+
+        for epoch in range(epochs):
+
+            print("{} epoch number: {}".format(datetime.now().strftime(TIME_FORMAT), epoch + 1))
+
+            # Training here
+            sess.run(train_init_op)
+
+            for batch in range(train_batches):
+
+                x_batch, y_batch = sess.run(next_batch)
+
+                feed_dict = dict(zip([*feeds], [x_batch, y_batch, True]))
+                _, _, obj_loss = sess.run([train_op, update_op, loss], feed_dict=feed_dict)
+
+                rel_loss = obj_loss / np.mean(np.square(y_batch))
+
+                if batch % display_step == 0:
+                    print("{}, epoch: {}, loss: {:.8f}, rel loss: {:.9f}"
+                          .format(datetime.now().strftime(TIME_FORMAT), epoch + 1, obj_loss, rel_loss))
+
+                    feed_dict = dict(zip([*feeds], [x_batch, y_batch, False]))
+                    s = sess.run(merged_summary, feed_dict=feed_dict)
+                    train_writer.add_summary(s, epoch * train_batches + batch)
+
+            # Validating here
+            print("{} begin validation".format(datetime.now().strftime(TIME_FORMAT)))
+
+            sess.run(val_init_op)
+
+            loss_list = []
+
+            for b in range(val_batches):
+
+                x_batch, y_batch = sess.run(next_batch)
+
+                feed_dict = dict(zip([*feeds], [x_batch, y_batch, False]))
+                obj_loss = sess.run([loss], feed_dict=feed_dict)
+
+                loss_list.append(obj_loss)
+
+                if b % display_step == 0:
+                    s = sess.run(merged_summary, feed_dict=feed_dict)
+                    dev_writer.add_summary(s, epoch * train_batches + b)
+
+            average_loss = np.mean(loss_list)
+            print("{}, epoch: {}, val loss: {:.8f}".format(datetime.now().strftime(TIME_FORMAT), epoch + 1, average_loss))
+
+            # # checkpoint save
+            checkpoint_name = os.path.join(logdir + '/ckpts', 'model_epoch' + str(epoch + 1) + '.ckpt')
+            saver.save(sess, checkpoint_name)
+            print("{} model checkpoint saved at {}".format(datetime.now().strftime(TIME_FORMAT), checkpoint_name))
+
+
+def get_file_list(files_path, save_file):
+    # 1st method
+    file_list = glob.glob(os.path.join(files_path, '*.png'))
+
+    # 2nd method
+    # only_files = [f for f in listdir(files_path) if isfile(join(files_path, f))]
+
+    with open(save_file, 'w') as f:
+        f.write('\n'.join(file_list))  # write(a single string) while writelines(list of string)
 
 
 
